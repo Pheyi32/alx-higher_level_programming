b48cd2ab44c7138f4011308e7fbccc05485aa7c2
Pheyi32 <feyisogoadeyemo.com>
2023-06-19 14:50:59 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
 """
-1-filter_states.py
-This file contains a script that lists all states with a
-name starting with N (upper N) from the database hbtn_0e_0_usa
+Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+Usage: ./1-filter_states.py <mysql username> \
+                             <mysql password> \
+                             <database name>
 """
-
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MySQLdb.connect("localhost", username, password, database)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states\
- WHERE name LIKE BINARY 'N%' ORDER BY id")
-    result = cursor.fetchall()
-    if result is not None:
-        for row in result:
-            print(row)
-
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
